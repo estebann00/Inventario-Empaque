ebbdaf413b994bf28548e8932b2c038311f8cd7d
estebann00 <99035216+estebann00@users.noreply.github.com>
2022-03-31 17:21:08 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,10 @@
     <!--<a href="{{route('registro.index')}}" class="border border-blue-800 rounded px-4 pt-1 h-10
       bg-blue-500 text-white font-semibold mx-2 no-underline">registro</a>-->
 
-    <a href="{{route('registro.vista')}}" class="border border-blue-800 rounded-md px-4 pt-1 h-10
-      bg-blue-500 text-white  mx-2 hover:bg-indigo-500 no-underline">Agregar</a>
+    <a href="{{route('registro.vista')}}" class="border border-blue-800 rounded-md px-3 pt-1 h-10
+      bg-blue-500 text-white  mx-1 hover:bg-indigo-500 no-underline">
+      <i class="fa-regular fa-square-plus"></i>
+      Agregar</a>
     </nav>
 
 <!-- Tabla de registro -->
@@ -18,36 +20,33 @@
         <table class="table-fixed-flex">
           <thead>
             <tr class="text-center bg-indigo-800 text-white">
-            <th class="col-1 py-3 ..."><i>Id</i></th>
-              <th class="col-1 py-3 ...">Empaque</th>
-              <th class="col-1 py-3 ...">Estado llegada</th>
-              <th class="col-3 py-2 ...">Remitente</th>
-              <th class="col-3 py-3 ...">Destinatario</th>
-              <th class="col-2 py-3 ...">Fecha llegada</th>
-              <th class="col-1 py-3 ...">Estado</th>
-              <th class="col-1 py-3 ...">Observacion</th>
+            <th class="col py-3 px-2 "><i>Id</i></th>
+              <th class="col-1 ">Empaque</th>
+              <th class="col-2 ">Estado llegada</th>
+              <th class="col-3 ">Remitente</th>
+              <th class="col-3">Destinatario</th>
+              <th class="col-2 ">Fecha llegada</th>
+              <th class="col-1">Estado</th>
+              <th class="col-1">Observacion</th>
             </tr>
           </thead>
           <tbody>
             @foreach ($productos as $product)
             <tr>
-            <td class="p-1 px-4">{{$product->id}}</td>
-              <td class="p-1 px-4">{{$product->pro_empaque}}</td>
-              <td class="p-1 px-4 ">{{$product->pro_estado_llegada}}</td>
-              <td class="p px-5">{{$product->pro_remitente}}</td>
-              <td class="p px-5">{{$product->pro_destinatario}}</td>
-              <td class="p px-5">{{$product->pro_fecha_entrada}}</td>
-              <td class="px-4">
+            <td class="px-4 py-3">{{$product->id}}</td>
+              <td class="px-4">{{$product->pro_empaque}}</td>
+              <td class="px-5 ">{{$product->pro_estado_llegada}}</td>
+              <td class="px-5">{{$product->pro_remitente}}</td>
+              <td class="px-5">{{$product->pro_destinatario}}</td>
+              <td class="px-5">{{$product->pro_fecha_entrada}}</td>
+              <td class="px-5">{{$product->estado->est_nombre}}</td>
+              <td class="">
               <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modaleditar{{$product->id}}">
-                detalle
+              <i class="fa-solid fa-pen-to-square"></i>
               </button>
-              </td>
 
-              <td class="p px-5">
-              <button type="button" data-bs-toggle="modal" data-bs-target="#modaldetalle{{$product->id}}"              
-			        class="uppercase p-3 flex items-center bg-blue-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg rounded-full w-12 h-12 ">
-				      <svg width="32" height="32"  viewBox="0 0 32 32" style="transform: rotate(360deg);"><path d="M29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29zM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9z" 
-              fill="currentColor"></path></svg>
+              <button type="button" data-bs-toggle="modal" data-bs-target="#modaldetalle{{$product->id}}" class="btn btn-primary">
+				      <i class="fa-solid fa-eye"></i>
               </button>
               @include ('auth.create')
 
@@ -81,7 +80,11 @@
               
               <h5>Observacion</h5>
               {{$product->pro_observacion}}
-              </div>      
+              </div>    
+
+              <h5>Estado</h5>
+              {{$product->estado->est_nombre}}
+              </div>  
 
                 <div class="modal-footer">  
                 <button type="button" class="bg-blue-500 0 text-white px-8 py-2 rounded-md text-1xl 
@@ -90,7 +93,7 @@
                 </div>
                     </div>
                   </div>
-                </div>
+                    </div>
                 </form>
               </td>
             </tr>
@@ -104,3 +107,4 @@
 
 
 @endsection
+      
