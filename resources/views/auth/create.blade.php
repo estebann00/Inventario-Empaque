b8dbf95f9cbd8327588dc2af4b2df9fc08ce4ada
estebann00 <99035216+estebann00@users.noreply.github.com>
2022-03-31 17:25:26 -0500

    Update create.blade.php

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -4,12 +4,23 @@
 <div class="modal fade" id="modaleditar{{$product->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-body" >
+            <div class="modal-body">
             <div class="mb-3">
             <div class="mb-2">
                 <label for="FormControlInput1" class="form-label">fecha salida</label>
                 <input type="date" class="form-control" name="pro_fecha_salida" value="{{$product->pro_fecha_salida}}" placeholder="fecha salida">
+            </div>            
+            <div class="form-group">
+              <select class="form-control" aria-label="Default select example" name="pro_estado">
+                  <option value="0" disabled selected> Estado </option>                    
+                    <option value="{{$product->estado->est_nombre}}">
+                    {{ucfirst($product->estado->est_nombre)}}
+                    </option>
+                    
+                   
+              </select>
             </div>
+
             <div class="modal-footer">  
                 <button type="button" class="bg-blue-500 0 text-white px-8 py-2 rounded-md text-1xl 
                         font-medium hover:bg-blue-700 transition duration-3" data-bs-dismiss="modal">Cerrar</button>
